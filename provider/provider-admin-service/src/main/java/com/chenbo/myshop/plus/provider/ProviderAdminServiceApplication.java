136f0e0aa831e85014c9e5fddfd17ac4c26ea113
r720 <963838470@qq.com>
2020-01-19 21:56:23 +0800

    配置异常

--- provider/provider-admin-service/src/main/java/com/chenbo/myshop/plus/provider/ProviderAdminServiceApplication.java
+++ provider/provider-admin-service/src/main/java/com/chenbo/myshop/plus/provider/ProviderAdminServiceApplication.java
@@ -1,11 +1,11 @@
 package com.chenbo.myshop.plus.provider;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.chenbo.myshop.plus.provider.service.mapper")
+@MapperScan(basePackages = "com.chenbo.myshop.plus.provider.mapper")
 public class ProviderAdminServiceApplication {
 
     public static void main(String[] args) {
