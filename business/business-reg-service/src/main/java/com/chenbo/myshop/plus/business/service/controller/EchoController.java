d1e3efceecd41aa8fb71eec0a1c6ae6c16ff880c
wugenshui <963838470@qq.com>
2020-01-19 17:08:22 +0800

    dubbo 服务注册消费

--- business/business-reg-service/src/main/java/com/chenbo/myshop/plus/business/service/controller/EchoController.java
+++ business/business-reg-service/src/main/java/com/chenbo/myshop/plus/business/service/controller/EchoController.java
@@ -1,5 +1,7 @@
 package com.chenbo.myshop.plus.business.service.controller;
 
+import com.chenbo.myshop.plus.provider.api.EchoService;
+import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "echo")
 public class EchoController {
-    //@Reference(version = "1.0.0")
-    //private EchoService echoService;
+    @Reference(version = "1.0.0")
+    private EchoService echoService;
 
     @GetMapping(value = "{string}")
     public String echo(@PathVariable String string) {
-        return "123"+string;
-        //return echoService.echo(string);
+        return echoService.echo(string);
     }
 }
---
+++ provider/provider-admin-service/src/main/java/com/chenbo/myshop/plus/provider/service/service/EchoServiceImpl.java
@@ -0,0 +1,16 @@
+package com.chenbo.myshop.plus.provider.service.service;
+
+import com.chenbo.myshop.plus.provider.api.EchoService;
+import org.apache.dubbo.config.annotation.Service;
+
+/**
+ * @author : chenbo
+ * @date : 2020-01-19
+ */
+@Service(version = "1.0.0")
+public class EchoServiceImpl implements EchoService {
+    @Override
+    public String echo(String string) {
+        return "Hello Dubbo" + string;
+    }
+}
