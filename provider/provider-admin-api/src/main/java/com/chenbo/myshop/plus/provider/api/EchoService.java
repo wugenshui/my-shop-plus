8e00f1bedddee4954ec407c902dd905fd23c477a
wugenshui <963838470@qq.com>
2020-01-19 15:09:36 +0800

    provider-admin-api

---
+++ provider/provider-admin-api/src/main/java/com/chenbo/myshop/plus/provider/api/EchoService.java
@@ -0,0 +1,17 @@
+package com.chenbo.myshop.plus.provider.api;
+
+/**
+ * @author : chenbo
+ * @date : 2020-01-19
+ */
+public interface EchoService {
+
+    /**
+     * 回声测试
+     *
+     * @param string 输入值
+     * @return 返回值
+     */
+    String echo(String string);
+
+}
